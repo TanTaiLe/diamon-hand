d278f2af80a1e81582cda8ae00d524df142e19e6
tantaile421 <tantai.le421@gmail.com>
2024-04-22 04:20:37 +0700

    done grade views + mine views

--- js/slider.js
+++ js/slider.js
@@ -2,9 +2,51 @@ import * as $ from './libs/jquery-3.7.1.slim.min.js';
 import * as slick from './libs/slick.min.js'
 
 jQuery(function () {
+
+  // Slider for promo views
   jQuery('#promoSlider').slick({
     dots: true,
     arrows: false,
     fade: true
   });
+
+  // Slider for grade
+  jQuery('.grade-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.grade-content',
+    centerMode: true,
+    focusOnSelect: true,
+    arrows: false,
+    infinite: false,
+    swipe: false
+  });
+  jQuery('.grade-content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.grade-content',
+    infinite: false,
+    swipe: false
+  });
+
+  // Slider for mine leaderboard
+  jQuery('.leaderboard-nav').slick({
+    slidesToScroll: 1,
+    asNavFor: '.leaderboard-content',
+    focusOnSelect: true,
+    infinite: false,
+    swipe: true,
+    fade: true,
+  });
+  jQuery('.leaderboard-content').slick({
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.leaderboard-content',
+    infinite: false,
+    swipe: false
+  });
 })
+
